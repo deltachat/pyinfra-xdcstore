fa443a11017712c25f0a3abd026015e9684ab2b0
missytake <missytake@systemli.org>
2023-09-24 15:51:02 +0200

    handle it if user doesn't provide environment variables

--- pyinfra_xdcstore/__init__.py
+++ pyinfra_xdcstore/__init__.py
@@ -2,6 +2,7 @@ import importlib.resources
 import os
 from io import StringIO
 
+import pyinfra
 from pyinfra.operations import files, systemd, server, git
 
 
@@ -14,9 +15,9 @@ def deploy_xdcstore(unix_user: str,
                     github_token=""):
     """Deploy xdcstore and xdcget to a UNIX user.
 
-    :param unix_user: the UNIX user of the bot
+    :param unix_user: the existing UNIX user of the bot
     :param bot_email: the email address for the bot account
-    :param bot_passwd: the password for the bot account
+    :param bot_passwd: the password for the bot's email account
     :param codeberg_user: the username of a codeberg account
     :param codeberg_token: an API token for the codeberg account
     :param github_user: the username of a github account
@@ -123,13 +124,18 @@ def deploy_xdcstore(unix_user: str,
 
 
 if __name__ == "__main__":
-    unix_user = os.getenv("XDCGET_UNIX_USER")
+    unix_user = os.getenv("XDCGET_UNIX_USER", "xdcstore")
     bot_email = os.getenv("XDCSTORE_EMAIL")
     bot_password = os.getenv("XDCSTORE_PASSWORD")
-    codeberg_user = os.getenv("XDCGET_CODEBERG_USER")
-    codeberg_token = os.getenv("XDCGET_CODEBERG_TOKEN")
-    github_user = os.getenv("XDCGET_GITHUB_USER")
-    github_token = os.getenv("XDCGET_GITHUB_TOKEN")
+    codeberg_user = os.getenv("XDCGET_CODEBERG_USER", "")
+    codeberg_token = os.getenv("XDCGET_CODEBERG_TOKEN", "")
+    github_user = os.getenv("XDCGET_GITHUB_USER", "")
+    github_token = os.getenv("XDCGET_GITHUB_TOKEN", "")
+
+    if bot_email is None:
+        pyinfra.logger.error("XDCSTORE_EMAIL can't be empty.")
+    if bot_password is None:
+        pyinfra.logger.error("XDCSTORE_PASSWORD can't be empty.")
 
     deploy_xdcstore(
         unix_user,
