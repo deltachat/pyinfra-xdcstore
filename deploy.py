aebcd5737e71e2802f55c6c72d87acdf29db2f22
link2xt <link2xt@testrun.org>
2023-09-28 12:02:05 +0000

    Move deploy.py code into main()
    Avoid creating global variables

--- deploy.py
+++ deploy.py
@@ -40,28 +40,32 @@ def create_unix_user(unix_user):
     )
 
 
-unix_user = os.getenv("XDCGET_UNIX_USER", "xdcstore")
-bot_email = os.getenv("XDCSTORE_EMAIL")
-bot_password = os.getenv("XDCSTORE_PASSWORD")
-codeberg_user = os.getenv("XDCGET_CODEBERG_USER", "")
-codeberg_token = os.getenv("XDCGET_CODEBERG_TOKEN", "")
-github_user = os.getenv("XDCGET_GITHUB_USER", "")
-github_token = os.getenv("XDCGET_GITHUB_TOKEN", "")
+def main():
+    unix_user = os.getenv("XDCGET_UNIX_USER", "xdcstore")
+    bot_email = os.getenv("XDCSTORE_EMAIL")
+    bot_password = os.getenv("XDCSTORE_PASSWORD")
+    codeberg_user = os.getenv("XDCGET_CODEBERG_USER", "")
+    codeberg_token = os.getenv("XDCGET_CODEBERG_TOKEN", "")
+    github_user = os.getenv("XDCGET_GITHUB_USER", "")
+    github_token = os.getenv("XDCGET_GITHUB_TOKEN", "")
 
-if bot_email is None:
-    pyinfra.logger.error("XDCSTORE_EMAIL can't be empty.")
-if bot_password is None:
-    pyinfra.logger.error("XDCSTORE_PASSWORD can't be empty.")
+    if bot_email is None:
+        pyinfra.logger.error("XDCSTORE_EMAIL can't be empty.")
+    if bot_password is None:
+        pyinfra.logger.error("XDCSTORE_PASSWORD can't be empty.")
 
-if unix_user not in [user for user in pyinfra.host.get_fact(Users)]:
-    create_unix_user(unix_user)
+    if unix_user not in [user for user in pyinfra.host.get_fact(Users)]:
+        create_unix_user(unix_user)
 
-deploy_xdcstore(
-    unix_user,
-    bot_email,
-    bot_password,
-    codeberg_user=codeberg_user,
-    codeberg_token=codeberg_token,
-    github_user=github_user,
-    github_token=github_token,
-)
+    deploy_xdcstore(
+        unix_user,
+        bot_email,
+        bot_password,
+        codeberg_user=codeberg_user,
+        codeberg_token=codeberg_token,
+        github_user=github_user,
+        github_token=github_token,
+    )
+
+
+main()
